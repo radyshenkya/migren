6443a76c21995839c9b62b3fed269f94e8a6c383
Sarygin Rodion <saryginrodion7@proton.me>
2025-08-06 18:15:08 +0700

    feat(MIG-14): Moved main to tokio-rs main

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ mod commands;
 mod env_args;
 
 use clap::Parser;
-use migren::default_migrations_dir;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let cli = cli_args::CliArgs::parse();
 
     let env_args = envy::from_env::<env_args::EnvArgs>()
