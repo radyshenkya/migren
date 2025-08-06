3c5d9472a14c79fdb62bc3f4ec557d99fa8adf6c
Sarygin Rodion <saryginrodion7@proton.me>
2025-08-06 18:42:41 +0700

    feat(MIG-13): set up logging

---
+++ src/errors.rs
@@ -0,0 +1,11 @@
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, MigrenError>;
+
+#[derive(Error, Debug)]
+pub enum MigrenError {
+    #[error("Failed to load env variables")]
+    Envy(#[from]envy::Error),
+    #[error("Failed to parse CLI arguments")]
+    Clap(#[from]clap::Error),
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 mod cli_args;
 mod commands;
+mod errors;
 mod env_args;
 
 use clap::Parser;
+use log::{info, error};
 
 #[tokio::main]
 async fn main() {
-    let cli = cli_args::CliArgs::parse();
+    env_logger::init();
 
-    let env_args = envy::from_env::<env_args::EnvArgs>()
-        .expect("Failed to parse environment variables");
+    let res: errors::Result<()> = (|| {
+        let cli = cli_args::CliArgs::parse();
+        info!(target: "main", "Hello world! {:?}", cli);
+
+        let env_args =
+            envy::from_env::<env_args::EnvArgs>()?;
+
+        Ok(())
+    })();
+
+    match res {
+        Ok(_) => {},
+        Err(err) => error!(target: "main", "Program failed: {:#?}", err),
+    }
 }
